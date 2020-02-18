3dce53e85ba359c1142b685c2eb5b85b442c6ff3
taannu <tavo.annus@gmail.com>
2020-02-18 19:44:49 +0200

    Initial menu wip

---
+++ app/src/main/java/ee/taltech/iti0213/dara/GameActivity.kt
@@ -0,0 +1,12 @@
+package ee.taltech.iti0213.dara
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
