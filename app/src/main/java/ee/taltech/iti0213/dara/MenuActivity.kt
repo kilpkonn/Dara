a94a31246e2a171eb9c4c9f9c92e8dfb1f563117
taannu <tavo.annus@gmail.com>
2020-02-19 21:58:45 +0200

    Add intent to start game activity

--- app/src/main/java/ee/taltech/iti0213/dara/MenuActivity.kt
+++ app/src/main/java/ee/taltech/iti0213/dara/MenuActivity.kt
@@ -1,12 +1,19 @@
 package ee.taltech.iti0213.dara
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+    }
+
+    fun onStartClicked(view: View) {
+        val startGameIntent = Intent(this, GameActivity::class.java)
+        startActivity(startGameIntent)
     }
 }
